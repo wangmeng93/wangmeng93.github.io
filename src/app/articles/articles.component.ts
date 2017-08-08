bb347aa56bfe5b166b7d2881a82ad3f4147af9f4
wangmeng <17071007515@163.com>
2017-08-08 10:09:28 +0800

    update

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -3,22 +3,30 @@
  */
 import {Component, OnInit} from '@angular/core';
 
-import {ArticlesService, ArticlesItems} from './shared/articles.service';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+import {ArticlesService, ArticlesItems,Items} from './shared/articles.service';
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
 export class ArticlesComponent implements OnInit {
-  constructor(private articlesService: ArticlesService) {
+  constructor(private articlesService: ArticlesService,
+              private domSanitizer: DomSanitizer) {
   }
 
   articlesItems: ArticlesItems[];
 
   ngOnInit() {
     this.articlesService.getArticlesItems()
-      .then(articlesItems =>{
+      .then(articlesItems => {
         this.articlesItems = articlesItems;
-      })
+      });
+
+
+    // this.articlesService.getArticles().subscribe(items =>{
+    //   this.items = items;
+    //   console.log(this.items);
+    // })
   }
 }
--- src/app/articles/shared/articles.service.ts
+++ src/app/articles/shared/articles.service.ts
@@ -7,16 +7,19 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 export class ArticlesItems {
-  constructor(public articleId: number,
-              public articleTitle: string,
-              public articleType: number) {
+  constructor(public title: string,
+              public url: string) {
   }
 }
 
 export class ArticleItem {
-  constructor(public articleId: number,
-              public articleTitle: string,
-              public articleType: number) {
+  constructor(public title: string,
+              public url: string) {
+  }
+}
+export class Items {
+  constructor(public title: string,
+              public url: string) {
   }
 }
 
@@ -29,29 +32,45 @@ export class ArticlesService {
   }
 
   getArticlesItems(): Promise<ArticlesItems[]> {
-    let articles = [{
-      articleId: 1,
-      articleTitle: '文章标题-1',
-      articleType: 1
-    }, {
-      articleId: 2,
-      articleTitle: '文章标题-2',
-      articleType: 1
-    }];
+
+    let articles = [
+      {
+        title: 'bootstrap弹出的模态框水平垂直居中的实现',
+        url: 'http://www.cnblogs.com/web-wangmeng/p/5616174.html'
+      },
+      {
+        title: 'angular简单的ui-view嵌套及传参',
+        url: 'http://www.cnblogs.com/web-wangmeng/p/6610827.html'
+      },
+      {
+        title: 'javascript 移动端h5页面自适应',
+        url: 'http://www.cnblogs.com/web-wangmeng/p/5641397.html'
+      },
+      {
+        title: 'angular组件component无法传递数据的坑',
+        url: 'http://www.cnblogs.com/web-wangmeng/p/6065202.html'
+      },
+      {
+        title: 'angular2 --开发环境搭建及项目创建',
+        url: 'http://www.cnblogs.com/web-wangmeng/p/6773911.html'
+      }
+    ];
     return Promise.resolve(articles);
   }
 
-  getArticleItem(articleId: number): Promise<any> {
-    let articles = [{
-      articleId: 1,
-      articleTitle: '文章标题-1',
-      articleType: 1
-    }, {
-      articleId: 2,
-      articleTitle: '文章标题-2',
-      articleType: 1
-    }];
-    return Promise.resolve(articles);
+  getArticles(): Observable<Items> {
+    let url= 'http://www.cnblogs.com/mvc/Blog/GetBlogSideBlocks.aspx?blogApp=web-wangmeng&showFlag=ShowRecentComment%2CShowTopViewPosts%2CShowTopFeedbackPosts%2CShowTopDiggPosts'
+
+    return this.http
+      .get(url)
+      .map(res => res.json() as Items)
+      .catch(this.handleError);
+
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log('error :', error);
+    return Observable.throw(error.message || error);
   }
 
 }
